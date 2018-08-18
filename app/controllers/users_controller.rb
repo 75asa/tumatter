cdafdd510c2703b59290f5d92f9a4e35c6223727
nasa <nuaskent@gmail.com>
2018-08-18 15:16:45 +0900

    add signup pages n error returns method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
+    if @user.save
+      # save method
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # 許可された項目だけを引数とする
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation)
+    end
 end
