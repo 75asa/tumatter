fc63d809336347e2fb60ae9bcf220832e713393f
nasa <nuaskent@gmail.com>
2018-08-18 15:54:55 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # save method
+      flash[:success] = "Welcom to Tumatter!!"
+      redirect_to @user
     else
       render 'new'
     end
