dc992012b68b7bf271c222707cb5be4ea3ab920b
nasa <nuaskent@gmail.com>
2018-08-20 11:39:28 +0900

    make fake users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       log_in user
       # 記憶トークンを生成しトークンのダイジェストをDBに保存
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       # エラーメッセージを作成
       flash.now[:danger] = 'Invalid email or password combination !!'
