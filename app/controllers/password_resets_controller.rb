80c66149461cc640e4b18b571d822c685bd92551
nasa <nuaskent@gmail.com>
2018-08-29 20:17:37 +0900

    add password-reset n little bug fixed

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,38 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :udpate]
+  before_action :valid_user, only: [:edit, :udpate]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "ops, email address not found"
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # 正しいユーザーかどうかを確認
+  def valid_user
+    unless (@user && @user.activated? &&
+          @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -57,6 +57,7 @@ class User < ApplicationRecord
     self.reset_token = User.new_token
     update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
+    # update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # パスワード再設定用のメールを送信
